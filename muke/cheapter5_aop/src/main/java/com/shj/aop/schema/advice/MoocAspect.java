f83e7f5c7e80945a2d82aee8c4b0ec9e75ed00a7
sunhaojie <sunhaojie02@meituan.com>
2019-08-07 15:49:17 +0800

    aop

--- muke/cheapter5_aop/src/main/java/com/shj/aop/schema/advice/MoocAspect.java
+++ muke/cheapter5_aop/src/main/java/com/shj/aop/schema/advice/MoocAspect.java
@@ -1,5 +1,8 @@
 package com.shj.aop.schema.advice;
 
+/**
+ * 切面方法类
+ */
 public class MoocAspect {
     public void before() {
         System.out.println(this.getClass().getName() + "切面前置通知： before");
--- muke/cheapter5_aop/src/main/java/com/shj/aop/schema/advice/biz/AspectBiz.java
+++ muke/cheapter5_aop/src/main/java/com/shj/aop/schema/advice/biz/AspectBiz.java
@@ -1,5 +1,8 @@
 package com.shj.aop.schema.advice.biz;
 
+/**
+ * 业务处理类
+ */
 public class AspectBiz {
     public void biz() {
         System.out.println(getClass().getName() + " biz(),  业务逻辑处理方法");
